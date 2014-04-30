d23f6a233191bbb13231d5dc034482e8be820a31
Danny DeBelius <danny.debelius@gmail.com>
2014-04-30 13:10:21 -0400

    More index style

--- www/js/app.js
+++ www/js/app.js
@@ -1,9 +1,4 @@
 $(function() {
-    // No box ad when we have adhesion, so #main-content gets 12 columns
-    if (window.innerWidth <= 1024){
-        $('#main-content').removeClass('col-md-8').addClass('col-md-12');
-    }
-
     // Templating example
     var context = $.extend(APP_CONFIG, {
         'template_path': 'jst/example.html',
