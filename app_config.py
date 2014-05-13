1c2778c1b08693ae236161534df4ff2544304233
Jeremy Bowers <jeremyjbowers@gmail.com>
2014-05-13 11:20:16 -0400

    Fixes sharing tools in the featured well; adds app_config support for sharing text.

--- app_config.py
+++ app_config.py
@@ -93,11 +93,13 @@ SHARE_URL = 'http://%s/%s/' % (PRODUCTION_S3_BUCKETS[0], PROJECT_SLUG)
 # Will be resized to 120x120, can't be larger than 1MB
 TWITTER_IMAGE_URL = '%squote-images/john-f-kennedy-yale-university-1962.png' % SHARE_URL
 TWITTER_HANDLE = '@nprviz'
+TWITTER_SHARE_TEXT = ''
 
 # 16:9 ("wide") image. FB uses 16:9 in the newsfeed and crops to square in timelines.
 # No documented restrictions on size
 FACEBOOK_IMAGE_URL = TWITTER_IMAGE_URL
 FACEBOOK_APP_ID = '138837436154588'
+FACEBOOK_SHARE_TEXT = ''
 
 # Thumbnail image for Google News / Search.
 # No documented restrictions on resolution or size
